6ebdb90c34ecc228f35373007e3ddeb9d3c8ab0e
Christophe Gouel <christophe.gouel@inra.fr>
2019-12-28 09:24:06 +0100

    Add dvi viewer for Windows

--- init.el
+++ init.el
@@ -340,7 +340,8 @@
 (if mswindows
     (progn
       (setq TeX-view-program-list (quote (("Sumatra PDF" ("\"C:/Program Files/SumatraPDF/SumatraPDF.exe\" -reuse-instance" (mode-io-correlate " -forward-search %b %n") " %o")))))
-      (setq TeX-view-program-selection (quote ((output-pdf "Sumatra PDF")))))
+      (setq TeX-view-program-selection (quote ((output-pdf "Sumatra PDF")
+					       (output-dvi "Yap")))))
     (progn
       (setq TeX-view-program-list '(("qpdfview" "qpdfview --instance emacsauxtex --unique \"%o#src:%b:%n:0\"")))
       (setq TeX-view-program-selection '((output-pdf "qpdfview")
