43c7f270feef3a8c48b42602df91977b9099e464
Christophe Gouel <christophe.gouel@inrae.fr>
2024-10-30 17:14:34 +0100

    Minor updates

--- init.el
+++ init.el
@@ -51,12 +51,8 @@
         display-time-24hr-format t ; Affichage de l'heure format 24h
         column-number-mode t ; affichage du numéro de la colonne
         prettify-symbols-unprettify-at-point t
-        show-trailing-whitespace t
         pixel-scroll-precision-mode t)
 (global-hl-line-mode +1) ; Highlight the current line
-(add-hook 'text-mode-hook 'prettify-symbols-mode)
-(add-hook 'prog-mode-hook (lambda ()
-                            (display-fill-column-indicator-mode)))
 (when (display-graphic-p)
   ;; Cursor (in terminal mode, to be set in terminal options)
   (setopt cursor-type 'bar) ; curseur étroit
@@ -74,7 +70,7 @@
   (set-face-attribute 'default nil :family "JetBrainsMono NF" :height 140)
   (setopt org-format-latex-options
             (plist-put org-format-latex-options :scale 1.8)
-          preview-scale-function 2))
+          preview-scale-function 1.4))
 (defun my-screen-default ()
   "Adjust font for default screen."
   (interactive)
@@ -366,6 +362,20 @@ current buffer within the project or the current directory if not in a project."
   :ensure nil
   :config
   (windmove-default-keybindings))
+
+(use-package prog-mode
+  :ensure nil
+  :defer t
+  :hook
+  (prog-mode . (lambda() (setq show-trailing-whitespace t)))
+  (prog-mode . (lambda () (display-fill-column-indicator-mode))))
+
+(use-package text-mode
+  :ensure nil
+  :defer t
+  :hook
+  (text-mode . (lambda() (setq show-trailing-whitespace t)))
+  (text-mode . prettify-symbols-mode))
 
 ;; Remove a bug appearing on Linux GTK and preventing the use of S-space (https://lists.gnu.org/archive/html/bug-gnu-emacs/2021-07/msg00071.html)
 (when (equal window-system 'pgtk)
@@ -809,15 +819,15 @@ current buffer within the project or the current directory if not in a project."
   (setq-default TeX-auto-parse-length 200
                 TeX-master nil)
   (add-hook 'TeX-after-compilation-finished-functions
-    	#'TeX-revert-document-buffer)
+	    #'TeX-revert-document-buffer)
 
   ;; To prevent TeX-view from jumping to the _region_.pdf file created by the
   ;; preview from
   ;; https://tex.stackexchange.com/questions/89399/auctex-how-to-jump-to-pdf-with-synctex-without-recompile-when-inline-preview
   (defun my-TeX-view-advice (orig-fun &rest args)
-    "Advice to ensure TeX-view always views the master file."
-    (let ((TeX-current-process-region-p nil))
-      (apply orig-fun args)))
+	"Advice to ensure TeX-view always views the master file."
+	(let ((TeX-current-process-region-p nil))
+	  (apply orig-fun args)))
   (advice-add 'TeX-view :around #'my-TeX-view-advice)
 
   (defun my-tex-compile ()
@@ -832,19 +842,19 @@ current buffer within the project or the current directory if not in a project."
     (interactive)
     (let (beg)
       (save-excursion
-    (search-backward "\\begin{frame}")
-    (setq beg (point))
-    (forward-char 1)
-    (LaTeX-find-matching-end)
-    (TeX-pin-region beg (point))
-    (cl-letf (( (symbol-function 'TeX-command-query) (lambda (x) "LaTeX")))
-      (TeX-command-region)))))
+	(search-backward "\\begin{frame}")
+	(setq beg (point))
+	(forward-char 1)
+	(LaTeX-find-matching-end)
+	(TeX-pin-region beg (point))
+	(cl-letf (( (symbol-function 'TeX-command-query) (lambda (x) "LaTeX")))
+	  (TeX-command-region)))))
   :bind
   (:map TeX-mode-map
-    ("C-c e" . TeX-next-error)
-    ("M-RET" . latex-insert-item)
-    ("S-<return>" . my-tex-frame)
-    ("<f9>" . my-tex-compile)))
+	("C-c e" . TeX-next-error)
+	("M-RET" . latex-insert-item)
+	("S-<return>" . my-tex-frame)
+	("<f9>" . my-tex-compile)))
 
 (use-package reftex
   :hook
