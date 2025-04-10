d6008c2d4190849606ce319d748d4a280a221ee3
Christophe Gouel <christophe.gouel@inrae.fr>
2025-04-10 10:39:17 +0200

    feat(audio): add helper functions for selecting audio devices and integrating whisper.el
    Introduce functions to list and select audio devices using ffmpeg on macOS. These include ~rk/get-ffmpeg-device~, ~rk/find-device-matching~, and ~rk/select-default-audio-device~. Additionally, integrate ~whisper.el~ with a custom command ~my-whisper-run~ for speech-to-text functionality.

--- init.el
+++ init.el
@@ -1480,6 +1480,91 @@ same directory as the working and insert a link to this file."
   :ensure-system-package
   (math-preview . "npm install -g git+https://gitlab.com/matsievskiysv/math-preview"))
 
+(defun rk/get-ffmpeg-device ()
+  "Gets the list of devices available to ffmpeg.
+The output of the ffmpeg command is pretty messy, e.g.
+  [AVFoundation indev @ 0x7f867f004580] AVFoundation video devices:
+  [AVFoundation indev @ 0x7f867f004580] [0] FaceTime HD Camera (Built-in)
+  [AVFoundation indev @ 0x7f867f004580] AVFoundation audio devices:
+  [AVFoundation indev @ 0x7f867f004580] [0] Cam Link 4K
+  [AVFoundation indev @ 0x7f867f004580] [1] MacBook Pro Microphone
+so we need to parse it to get the list of devices.
+The return value contains two lists, one for video devices and one for audio devices.
+Each list contains a list of cons cells, where the car is the device number and the cdr is the device name."
+  (unless (string-equal system-type "darwin")
+    (error "This function is currently only supported on macOS"))
+
+  (let ((lines (string-split (shell-command-to-string "ffmpeg -list_devices true -f avfoundation -i dummy || true") "\n")))
+    (cl-loop with at-video-devices = nil
+             with at-audio-devices = nil
+             with video-devices = nil
+             with audio-devices = nil
+             for line in lines
+             when (string-match "AVFoundation video devices:" line)
+             do (setq at-video-devices t
+                      at-audio-devices nil)
+             when (string-match "AVFoundation audio devices:" line)
+             do (setq at-audio-devices t
+                      at-video-devices nil)
+             when (and at-video-devices
+                       (string-match "\\[\\([0-9]+\\)\\] \\(.+\\)" line))
+             do (push (cons (string-to-number (match-string 1 line)) (match-string 2 line)) video-devices)
+             when (and at-audio-devices
+                       (string-match "\\[\\([0-9]+\\)\\] \\(.+\\)" line))
+             do (push (cons (string-to-number (match-string 1 line)) (match-string 2 line)) audio-devices)
+             finally return (list (nreverse video-devices) (nreverse audio-devices)))))
+
+(defun rk/find-device-matching (string type)
+  "Get the devices from `rk/get-ffmpeg-device' and look for a device
+matching `STRING'. `TYPE' can be :video or :audio."
+  (let* ((devices (rk/get-ffmpeg-device))
+         (device-list (if (eq type :video)
+                          (car devices)
+                        (cadr devices))))
+    (cl-loop for device in device-list
+             when (string-match-p string (cdr device))
+             return (car device))))
+
+(defcustom rk/default-audio-device nil
+  "The default audio device to use for whisper.el and outher audio processes."
+  :type 'string)
+
+(defun rk/select-default-audio-device (&optional device-name)
+  "Interactively select an audio device to use for whisper.el and other audio processes.
+If `DEVICE-NAME' is provided, it will be used instead of prompting the user."
+  (interactive)
+  (let* ((audio-devices (cadr (rk/get-ffmpeg-device)))
+         (indexes (mapcar #'car audio-devices))
+         (names (mapcar #'cdr audio-devices))
+         (name (or device-name (completing-read "Select audio device: " names nil t))))
+    (setq rk/default-audio-device (rk/find-device-matching name :audio))
+    (when (boundp 'whisper--ffmpeg-input-device)
+      (setq whisper--ffmpeg-input-device (format ":%s" rk/default-audio-device)))))
+
+(use-package whisper
+  :ensure t
+  :vc (:url "https://github.com/natrys/whisper.el"
+       :rev :newest
+       :branch "main")
+  :config
+  (defun my-whisper-run ()
+    "Check input device and run whisper.
+If no specific input device is set, or if called with a universal argument (C-u),
+the function will prompt the user to select a default audio device before running whisper."
+    (interactive)
+    (if (or (null whisper--ffmpeg-input-device) current-prefix-arg)
+	(progn
+          (rk/select-default-audio-device)
+          (whisper-run))
+      (whisper-run)))
+  :bind ("C-c w" . my-whisper-run)
+  :custom
+  (whisper-install-directory "/tmp/")
+  (whisper-model "base")
+  (whisper-language "en")
+  (whisper-translate nil)
+  (whisper-use-threads (/ (num-processors) 2)))
+
 (use-package flyspell
   :ensure t
   :hook (text-mode . flyspell-mode)
